c10996686c8325c4b85c2f17fd7d2861dc9c9668
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-02-07 17:22:47 +0100

    retrained with new model attri names

--- test.py
+++ test.py
@@ -6,3 +6,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 from model import Classifier
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = Classifier()
+model.to(device)
+
+model.load_state_dict(torch.load("tomjerry.pth", weights_only=True))
+model.eval()
+
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr = 1e-4
+)
+
+transform = transforms.Compose([transforms.Resize((227, 227)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize([0.5, 0.5, 0.5],
+                                                     [0.5, 0.5, 0.5])])
+
+
+class_names = ['jerry', 'tom', 'none', 'both']  # adjust if needed
+
