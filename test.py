fd6d9a4ac22e33db24ecd466701dab70ae8c5b66
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-02-07 15:46:40 +0100

    add test file

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from model import Classifier
